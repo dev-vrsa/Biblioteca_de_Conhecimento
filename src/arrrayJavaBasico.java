5577344a59ef8b400e70da4e9e7945d4b8a6169b
victor <victor.rsa@mercadolivre.com>
2023-01-11 13:40:06 -0300

    Atualizando Biblioteca

---
+++ src/arrrayJavaBasico.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class arrrayJavaBasico {
+
+    public static void main(String[] args) {
+        
+        ArrayList <String> MeuPrimeiroArray  = new ArrayList<String>();
+
+        MeuPrimeiroArray.add("Victor");
+        MeuPrimeiroArray.add("Aurora");
+        MeuPrimeiroArray.add("Divina");
+
+        System.out.println("                                          ");
+        System.out.println("                                          ");
+        System.out.println(MeuPrimeiroArray);
+        System.out.println("                                          ");
+        System.out.println("                                          ");
+        
+
+
+    }
+    
+}
